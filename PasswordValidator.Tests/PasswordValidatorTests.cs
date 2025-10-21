ec19309edfad05499ed174631590b65c2c6315aa
221 <221@221-01>
2025-10-21 13:36:30 +0300

    +

--- PasswordValidator.Tests/PasswordValidatorTests.cs
+++ PasswordValidator.Tests/PasswordValidatorTests.cs
@@ -1,11 +1,12 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using PasswordValidator;
+using Assert = Xunit.Assert;
 
 namespace PasswordValidator.Tests
 {
-    [TestClass]
     public class PasswordValidatorTests
     {
-        [TestMethod]
+        [Fact]
         public void ValidatePassword_ValidPassword_ReturnsTrue()
         {
             // Arrange
@@ -15,10 +16,10 @@ namespace PasswordValidator.Tests
             bool result = PasswordValidator.ValidatePassword(validPassword);
 
             // Assert
-            Assert.IsTrue(result);
+            Assert.True(result);
         }
 
-        [TestMethod]
+        [Fact]
         public void ValidatePassword_TooShort_ReturnsFalse()
         {
             // Arrange
@@ -28,10 +29,10 @@ namespace PasswordValidator.Tests
             bool result = PasswordValidator.ValidatePassword(shortPassword);
 
             // Assert
-            Assert.IsFalse(result);
+            Assert.False(result);
         }
 
-        [TestMethod]
+        [Fact]
         public void ValidatePassword_NoDigits_ReturnsFalse()
         {
             // Arrange
@@ -41,10 +42,10 @@ namespace PasswordValidator.Tests
             bool result = PasswordValidator.ValidatePassword(noDigitPassword);
 
             // Assert
-            Assert.IsFalse(result);
+            Assert.False(result);
         }
 
-        [TestMethod]
+        [Fact]
         public void ValidatePassword_NoLetters_ReturnsFalse()
         {
             // Arrange
@@ -54,10 +55,10 @@ namespace PasswordValidator.Tests
             bool result = PasswordValidator.ValidatePassword(noLetterPassword);
 
             // Assert
-            Assert.IsFalse(result);
+            Assert.False(result);
         }
 
-        [TestMethod]
+        [Fact]
         public void ValidatePassword_EmptyString_ReturnsFalse()
         {
             // Arrange
@@ -67,10 +68,10 @@ namespace PasswordValidator.Tests
             bool result = PasswordValidator.ValidatePassword(emptyPassword);
 
             // Assert
-            Assert.IsFalse(result);
+            Assert.False(result);
         }
 
-        [TestMethod]
+        [Fact]
         public void ValidatePassword_Null_ReturnsFalse()
         {
             // Arrange
@@ -80,7 +81,23 @@ namespace PasswordValidator.Tests
             bool result = PasswordValidator.ValidatePassword(nullPassword);
 
             // Assert
-            Assert.IsFalse(result);
+            Assert.False(result);
+        }
+
+        // “есты с использованием Theory дл€ параметризованного тестировани€
+        [Theory]
+        [InlineData("Pass1234", true)] // валидный пароль
+        [InlineData("short1", false)] // слишком короткий
+        [InlineData("NoDigitsHere", false)] // нет цифр
+        [InlineData("12345678", false)] // нет букв
+        [InlineData("", false)] // пуста€ строка
+        public void ValidatePassword_ParameterizedTests(string password, bool expected)
+        {
+            // Act
+            bool result = PasswordValidator.ValidatePassword(password);
+
+            // Assert
+            Assert.Equal(expected, result);
         }
     }
 }
